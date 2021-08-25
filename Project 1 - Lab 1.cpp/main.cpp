763c8decd93ca1c557130cac2d3ad145b14f0ae2
BennettN1 <89536101+BennettN1@users.noreply.github.com>
2021-08-25 18:46:31 -0500

    Initial Commit

---
+++ Project 1 - Lab 1.cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project 1 - Lab 1.cpp
+//
+//  Created by Nicholas Bennett on 8/25/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
