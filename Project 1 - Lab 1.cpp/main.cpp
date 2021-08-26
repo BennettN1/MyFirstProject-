d8b7565f1a5420951a4c9f72087533b96ba42581
BennettN1 <89536101+BennettN1@users.noreply.github.com>
2021-08-25 21:16:22 -0500

    Update main.cpp

--- Project 1 - Lab 1.cpp/main.cpp
+++ Project 1 - Lab 1.cpp/main.cpp
@@ -2,7 +2,7 @@
  Nick Bennett
  C++ 2021
  Lab 1 and Hello World
- Figuring out the basics of writing code and understanding what is going on inside of each piece of data that is used
+ Figuring out the basics of writing code in C++ and understanding what is going on inside of each piece of data that is used inside of the code. 
  */
  
 #include <iostream>
